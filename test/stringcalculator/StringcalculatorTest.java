421bded676373fca501f5bccaec165985e329845
Heet Kalaria <53978492+heetkalaria@users.noreply.github.com>
2021-08-30 00:04:30 +0530

    Working with Empty String

---
+++ test/stringcalculator/StringcalculatorTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringcalculator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Heet
+ */
+public class StringcalculatorTest {
+
+    @Test
+    public void testEmptyString() {
+        assertEquals(0, Stringcalculator.add(""));
+    }
+
+}
